6efc1cb42928764ea0c3835603df5a43823e94ae
omerSuliman <omerahmed41@gmail.com>
2022-08-02 15:52:39 +0400

    - project init. - setup spring-boot - configure docker - configure makefile

---
+++ Makefile
@@ -0,0 +1,13 @@
+build:
+	docker-compose build --pull --no-cache
+
+run:
+	 docker-compose up
+reload:
+	make stop; docker-compose build; make run
+stop:
+	make shutdownApp; docker compose down
+
+shutdownApp:
+	@curl -X POST http://localhost:8085/actuator/shutdown
+
---
+++ payment/src/main/java/com/hitpixel/payment/PaymentApplication.java
@@ -0,0 +1,25 @@
+package com.hitpixel.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+@SpringBootApplication
+@RestController
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello 2!";
+	}
+
+}
