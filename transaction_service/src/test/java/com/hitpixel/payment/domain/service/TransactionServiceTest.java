faf8673dce2092e5ff95f4f9c4659714f92f1af9
omerSuliman <omerahmed41@gmail.com>
2022-08-07 06:35:16 +0400

    - update transaction service tests.

--- transaction_service/src/test/java/com/hitpixel/payment/domain/service/TransactionServiceTest.java
+++ transaction_service/src/test/java/com/hitpixel/payment/domain/service/TransactionServiceTest.java
@@ -1,33 +1,22 @@
 package com.hitpixel.payment.domain.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.hitpixel.payment.Infrastructure.repository.TransactionRepository;
 import com.hitpixel.payment.domain.entity.Transaction;
-
-import java.time.LocalDateTime;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.junit.jupiter.api.Disabled;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {TransactionService.class})
 @ExtendWith(SpringExtension.class)
@@ -44,288 +33,141 @@ class TransactionServiceTest {
     @Autowired
     private TransactionService transactionService;
 
-    /**
-     * Method under test: {@link TransactionService#saveTransaction(Transaction)}
-     */
     @Test
-    void testSaveTransaction() {
+    @DisplayName("Should return transaction when transaction status is approved")
+    void refundDoneWhenTransactionStatusIsApprovedThenReturnTransaction() {
         Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
         transaction.setTransactionID(1L);
-        when(transactionRepository.save((Transaction) any())).thenReturn(transaction);
-        doNothing().when(messagePublisher).publishTransactionCreatedMessage((Transaction) any());
-
-        Transaction transaction1 = new Transaction();
-        transaction1.setAmount(10L);
-        transaction1.setCard_type("Card type");
-        transaction1.setClientID(1L);
-        transaction1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction1.setCurrency("GBP");
-        transaction1.setEmail("jane.doe@example.org");
-        transaction1.setOrderID(1L);
-        transaction1.setStatus("Status");
-        transaction1.setTransactionID(1L);
-        assertSame(transaction, transactionService.saveTransaction(transaction1));
-        verify(transactionRepository).save((Transaction) any());
-        verify(messagePublisher).publishTransactionCreatedMessage((Transaction) any());
-    }
-
-    /**
-     * Method under test: {@link TransactionService#getAllTransaction()}
-     */
-    @Test
-    void testGetAllTransaction() {
-        when(transactionRepository.findAll()).thenReturn(new ArrayList<>());
-        ResponseEntity<List<Transaction>> actualAllTransaction = transactionService.getAllTransaction();
-        assertTrue(actualAllTransaction.hasBody());
-        assertEquals(HttpStatus.OK, actualAllTransaction.getStatusCode());
-        assertTrue(actualAllTransaction.getHeaders().isEmpty());
-        verify(transactionRepository).findAll();
-    }
-
-    /**
-     * Method under test: {@link TransactionService#getTransaction(Long)}
-     */
-    @Test
-    void testGetTransaction() {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-        assertSame(transaction, transactionService.getTransaction(1L));
-        verify(transactionRepository).findById((Long) any());
-    }
-
-    /**
-     * Method under test: {@link TransactionService#getTransaction(Long)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testGetTransaction2() {
-        // TODO: Complete this test.
-
-        when(transactionRepository.findById((Long) any())).thenReturn(Optional.empty());
-        transactionService.getTransaction(1L);
-    }
-
-    /**
-     * Method under test: {@link TransactionService#deleteTransaction(long)}
-     */
-    @Test
-    void testDeleteTransaction() {
-        doNothing().when(transactionRepository).deleteById((Long) any());
-        assertEquals("Transaction removed !! 123", transactionService.deleteTransaction(123L));
-        verify(transactionRepository).deleteById((Long) any());
-    }
-
-    /**
-     * Method under test: {@link TransactionService#updateTransaction(long, Transaction)}
-     */
-    @Test
-    void testUpdateTransaction() {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
-
-        Transaction transaction1 = new Transaction();
-        transaction1.setAmount(10L);
-        transaction1.setCard_type("Card type");
-        transaction1.setClientID(1L);
-        transaction1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction1.setCurrency("GBP");
-        transaction1.setEmail("jane.doe@example.org");
-        transaction1.setOrderID(1L);
-        transaction1.setStatus("Status");
-        transaction1.setTransactionID(1L);
-        when(transactionRepository.save((Transaction) any())).thenReturn(transaction1);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-
-        Transaction transaction2 = new Transaction();
-        transaction2.setAmount(10L);
-        transaction2.setCard_type("Card type");
-        transaction2.setClientID(1L);
-        transaction2.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction2.setCurrency("GBP");
-        transaction2.setEmail("jane.doe@example.org");
-        transaction2.setOrderID(1L);
-        transaction2.setStatus("Status");
-        transaction2.setTransactionID(1L);
-        assertSame(transaction1, transactionService.updateTransaction(123L, transaction2));
-        verify(transactionRepository).save((Transaction) any());
-        verify(transactionRepository).findById((Long) any());
-    }
-
-    /**
-     * Method under test: {@link TransactionService#updateTransaction(long, Transaction)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testUpdateTransaction2() {
-        // TODO: Complete this test.
-
-
-    }
-
-    /**
-     * Method under test: {@link TransactionService#refundRequest(Long)}
-     */
-    @Test
-    void testRefundRequest() {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-        assertSame(transaction, transactionService.refundRequest(1L));
-        verify(transactionRepository).findById((Long) any());
-    }
-
-    /**
-     * Method under test: {@link TransactionService#refundRequest(Long)}
-     */
-    @Test
-    void testRefundRequest2() {
-        Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
         transaction.setStatus("approved");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-        doNothing().when(messagePublisher).publishTransactionRefundRequestedMessage((Transaction) any());
-        assertSame(transaction, transactionService.refundRequest(1L));
-        verify(transactionRepository).findById((Long) any());
-        verify(messagePublisher).publishTransactionRefundRequestedMessage((Transaction) any());
+        when(transactionRepository.findById(anyLong()))
+                .thenReturn(java.util.Optional.of(transaction));
+        Transaction refundedTransaction = transactionService.refundDone(1L);
+        assertEquals("refunded", refundedTransaction.getStatus());
     }
 
-    /**
-     * Method under test: {@link TransactionService#refundRequest(Long)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
-    void testRefundRequest3() {
-
-
-        when(transactionRepository.findById((Long) any())).thenReturn(Optional.empty());
-        doNothing().when(messagePublisher).publishTransactionRefundRequestedMessage((Transaction) any());
-        transactionService.refundRequest(1L);
-    }
-
-    /**
-     * Method under test: {@link TransactionService#refundDone(Long)}
-     */
-    @Test
-    void testRefundDone() {
+    @DisplayName("Should return transaction when transaction status is not approved")
+    void refundDoneWhenTransactionStatusIsNotApprovedThenReturnTransaction() {
         Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-        assertSame(transaction, transactionService.refundDone(1L));
-        verify(transactionRepository).findById((Long) any());
+        transaction.setStatus("not_approved");
+        when(transactionRepository.findById(anyLong()))
+                .thenReturn(java.util.Optional.of(transaction));
+        Transaction result = transactionService.refundDone(1L);
+        assertEquals(transaction, result);
     }
 
-    /**
-     * Method under test: {@link TransactionService#refundDone(Long)}
-     */
     @Test
-    void testRefundDone2() {
+    @DisplayName("Should throw exception when transaction is not found")
+    void refundRequestWhenTransactionIsNotFoundThenThrowException() {
+        when(transactionRepository.findById(anyLong())).thenReturn(null);
+        assertThrows(RuntimeException.class, () -> transactionService.refundRequest(1L));
+    }
+
+    @Test
+    @DisplayName("Should delete the transaction when the id is valid")
+    void deleteTransactionWhenIdIsValid() {
+        long id = 1L;
         Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
+        transaction.setTransactionID(id);
+        transaction.setEmail("test@test.com");
+        transaction.setAmount(100L);
+        transaction.setCurrency("USD");
         transaction.setStatus("approved");
-        transaction.setTransactionID(1L);
-        Optional<Transaction> ofResult = Optional.of(transaction);
+        transaction.setCreatedAt(LocalDateTime.now());
 
-        Transaction transaction1 = new Transaction();
-        transaction1.setAmount(10L);
-        transaction1.setCard_type("Card type");
-        transaction1.setClientID(1L);
-        transaction1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction1.setCurrency("GBP");
-        transaction1.setEmail("jane.doe@example.org");
-        transaction1.setOrderID(1L);
-        transaction1.setStatus("Status");
-        transaction1.setTransactionID(1L);
-        when(transactionRepository.save((Transaction) any())).thenReturn(transaction1);
-        when(transactionRepository.findById((Long) any())).thenReturn(ofResult);
-        doNothing().when(messagePublisher).publishTransactionRefundDoneMessage((Transaction) any());
-        Transaction actualRefundDoneResult = transactionService.refundDone(1L);
-        assertSame(transaction, actualRefundDoneResult);
-        assertEquals("refunded", actualRefundDoneResult.getStatus());
-        verify(transactionRepository).save((Transaction) any());
-        verify(transactionRepository).findById((Long) any());
-        verify(messagePublisher).publishTransactionRefundDoneMessage((Transaction) any());
+        when(transactionRepository.findById(id)).thenReturn(java.util.Optional.of(transaction));
+
+        String result = transactionService.deleteTransaction(id);
+
+        assertEquals("Transaction removed !! 1", result);
     }
 
-    /**
-     * Method under test: {@link TransactionService#refundDone(Long)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
-    void testRefundDone3() {
+    @DisplayName("Should return the transaction when the transaction exists")
+    void getTransactionWhenTransactionExists() {
+        Transaction transaction =
+                new Transaction(
+                        1L,
+                        1L,
+                        1L,
+                        "test@test.com",
+                        LocalDateTime.now(),
+                        100L,
+                        "USD",
+                        "VISA",
+                        "approved");
+        when(transactionRepository.findById(1L)).thenReturn(java.util.Optional.of(transaction));
+        Transaction result = transactionService.getTransaction(1L);
+        assertSame(transaction, result);
+    }
 
+    @Test
+    @DisplayName("Should throw an exception when the transaction does not exist")
+    void getTransactionWhenTransactionDoesNotExistThenThrowsException() {
+        when(transactionRepository.findById(1L)).thenReturn(null);
+        assertThrows(RuntimeException.class, () -> transactionService.getTransaction(1L));
+    }
+
+    @Test
+    @DisplayName("Should return all transactions")
+    void getAllTransactionShouldReturnAllTransactions() {
+        Transaction transaction =
+                new Transaction(
+                        1L,
+                        1L,
+                        1L,
+                        "test@test.com",
+                        LocalDateTime.now(),
+                        100L,
+                        "USD",
+                        "VISA",
+                        "approved");
+        when(transactionRepository.findAll()).thenReturn(List.of(transaction));
+        assertSame(transactionService.getAllTransaction().getBody().get(0), transaction);
+    }
+
+    @Test
+    @DisplayName("Should save the transaction")
+    void saveTransaction() {
         Transaction transaction = new Transaction();
-        transaction.setAmount(10L);
-        transaction.setCard_type("Card type");
-        transaction.setClientID(1L);
-        transaction.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
-        transaction.setCurrency("GBP");
-        transaction.setEmail("jane.doe@example.org");
-        transaction.setOrderID(1L);
-        transaction.setStatus("Status");
         transaction.setTransactionID(1L);
-        when(transactionRepository.save((Transaction) any())).thenReturn(transaction);
-        when(transactionRepository.findById((Long) any())).thenReturn(Optional.empty());
-        doNothing().when(messagePublisher).publishTransactionRefundDoneMessage((Transaction) any());
-        transactionService.refundDone(1L);
+        transaction.setOrderID(1L);
+        transaction.setClientID(1L);
+        transaction.setEmail("test@test.com");
+        transaction.setCreatedAt(LocalDateTime.now());
+        transaction.setAmount(100L);
+        transaction.setCurrency("USD");
+        transaction.setCard_type("VISA");
+        transaction.setStatus("approved");
+
+        when(transactionRepository.save(transaction)).thenReturn(transaction);
+
+        Transaction savedTransaction = transactionService.saveTransaction(transaction);
+
+        assertEquals(savedTransaction, transaction);
+    }
+
+    @Test
+    @DisplayName("Should publish a message")
+    void saveTransactionShouldPublishMessage() {
+        Transaction transaction = new Transaction();
+        transaction.setTransactionID(1L);
+        transaction.setEmail("test@test.com");
+        transaction.setCreatedAt(LocalDateTime.now());
+        transaction.setAmount(100L);
+        transaction.setCurrency("EUR");
+        transaction.setCard_type("VISA");
+        transaction.setStatus("approved");
+
+        when(transactionRepository.save(transaction)).thenReturn(transaction);
+
+        doNothing().when(messagePublisher).publishTransactionCreatedMessage(transaction);
+
+        Transaction saved_transaction = transactionService.saveTransaction(transaction);
+
+        assertSame(saved_transaction, transaction);
+
+        verify(messagePublisher, times(1)).publishTransactionCreatedMessage(transaction);
     }
 }
 
